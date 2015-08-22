deacd412347825b425d4c29a6908998eeb61cfd5
Stephan Florquin <stephan.florquin@gmail.com>
2015-08-22 15:55:34 +0200

    Update redux-smart-action to 0.2.0 and make actions not branch

--- src/Actions.js
+++ src/Actions.js
@@ -5,29 +5,29 @@ export const undo = () => new SmartAction((dispatch, getState) => {
   if (getState().undoStack.pos > 0) {
     dispatch({type: UNDO});
   }
-});
+}, false);
 
 export const redo = () => new SmartAction((dispatch, getState) => {
   const state = getState();
   if (state.undoStack.pos < state.undoStack.stack.length) {
     dispatch({type: REDO});
   }
-});
+}, false);
 
 export const begin = desc => new SmartAction((dispatch, getState) => {
   if (!getState().undoStack.entry) {
     dispatch({type: BEGIN, desc});
   }
-});
+}, false);
 
 export const commit = () => new SmartAction((dispatch, getState) => {
   if (getState().undoStack.entry) {
     dispatch({type: COMMIT});
   }
-});
+}, false);
 
 export const abort = () => new SmartAction((dispatch, getState) => {
   if (getState().undoStack.entry) {
     dispatch({type: ABORT});
   }
-});
+}, false);
